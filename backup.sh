292122b7a2abfa121287fb01162d9a46de7a5a36
raqso <raqsasg@gmail.com>
2020-04-03 22:11:25 +0200

    Make script output optional
    Changed echo calls to the log function calls

--- backup.sh
+++ backup.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+
+VERBOSE=false
+
+case "$1" in
+  -v | --verbose ) VERBOSE=true ;;
+esac
+
+function log() {
+
+	if [ "$VERBOSE" = true ]; then
+		echo -e "${1}"
+
+	fi
+}
+
 cd "$(dirname "$0")"
 
 if [ -f config.conf ];then
   . ./config.conf
 else
-  echo "No configuration file!"
+  log "No configuration file!"
   exit 1
 fi
 
@@ -13,22 +28,22 @@ TO_REMOVE=`date +%F -d"-$DAYS day"`
 
 if [ -e $BACKUPS_DIRECTORY/$TO_REMOVE.gz ];then
   rm $BACKUPS_DIRECTORY/$TO_REMOVE.gz
-  echo "[0] Oldest backup removed..."
+  log "[0] Oldest backup removed..."
 fi
 
 tar cpvfP $BACKUPS_DIRECTORY/www.tar $WWW_DIRECTORY
-echo "[1] Home directory packed..."
+log "[1] Home directory packed..."
 
 mysqldump $DATABASE -u$USER -p$PASS > $BACKUPS_DIRECTORY/$DATABASE.sql
-echo "[2] Database file downloaded..."
+log "[2] Database file downloaded..."
 
 env GZIP=-9 tar cfvz $BACKUPS_DIRECTORY/$CURRENT_BACKUP.gz $BACKUPS_DIRECTORY/www.tar $BACKUPS_DIRECTORY/$DATABASE.sql
-echo "[3] Database file compressed..."
+log "[3] Database file compressed..."
 
 rm $BACKUPS_DIRECTORY/www.tar $BACKUPS_DIRECTORY/$DATABASE.sql
-echo "[3] Files and database packed into one archive..."
+log "[3] Files and database packed into one archive..."
 
 ./upload.sh -v -r $GDRIVE_DIRECTORY $BACKUPS_DIRECTORY/$CURRENT_BACKUP.gz
-echo "[4] Backup file uploaded to Google Drive!"
+log "[4] Backup file uploaded to Google Drive!"
 
 exit
